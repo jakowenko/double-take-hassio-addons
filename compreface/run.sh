f008e90a07e3ead2f46cf6690429d6e420b3f8fc
David Jakowenko <jakowenko@gmail.com>
2021-11-28 02:26:43 -0500

    feat(compreface): ability to change postgres env variables

--- compreface/run.sh
+++ compreface/run.sh
@@ -4,11 +4,15 @@
 #
 # Ensure persistent data is stored in /data/ and then start the stack
 
+values=`cat /data/options.json`
+for s in $(echo $values | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
+    export $s
+done
+
 if [ "$PGDATA" == "/data/database" ] && [ -d /data ]
 then
    if [ ! -d  /data/database ]
    then
-      # Populate
       cp -rp /var/lib/postgresql/data /data/database
    fi
 fi
